034280ab9130b7012ce6bade799b5cc021ee4f31
ronanduddy <ronanduddy@live.ie>
2018-02-28 19:14:23 +0000

    Add all items component

--- app/assets/javascripts/components/_all_items.js.jsx
+++ app/assets/javascripts/components/_all_items.js.jsx
@@ -1,9 +1,33 @@
 class AllItems extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: []
+    };
+  }
+
+
   componentDidMount() {
-    console.log('Component mounted');
+    this.getItems();
+  }
+
+  getItems(){
+    $.getJSON(
+      '/api/v1/items.json',
+      (response) => { this.setState({ items: response }) }
+    );
   }
 
   render() {
-    return <div> <h2>All Items</h2> </div>;
+    const items = this.state.items.map((item) => {
+      return (
+        <div key={item.id}> {/* key is important for list of similar elements */}
+          <h3>#{item.id} {item.name}</h3>
+          <p>{item.description}</p>
+        </div>
+      )
+    });
+    return <div> <h2>All Items</h2> {items} </div>;
   }
 }
