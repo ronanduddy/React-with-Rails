427fcb4b756e0ad378506d8360e5e9efbc0e9af4
ronanduddy <ronanduddy@live.ie>
2018-03-01 20:30:49 +0000

    Add item deletion functionality

--- app/assets/javascripts/components/_all_items.js.jsx
+++ app/assets/javascripts/components/_all_items.js.jsx
@@ -4,15 +4,27 @@ class AllItems extends React.Component {
     super(props);
   }
 
+  handleDelete(id){
+    this.props.handleItemDeletion(id);
+  }
+
   render() {
     const items = this.props.items.map((item) => {
       return (
         <div key={item.id}> {/* key is important for list of similar elements */}
           <h3>#{item.id} {item.name}</h3>
           <p>{item.description}</p>
+          <button onClick={this.handleDelete.bind(this, item.id)}>Delete</button>
         </div>
       )
     });
-    return <div> <h2>All Items (x{items.length})</h2> {items} </div>;
+
+    return (
+      <div>
+        <h2>All Items (x{items.length})</h2>
+        {items}
+      </div>
+    );
   }
+
 }
--- app/assets/javascripts/components/_body.js.jsx
+++ app/assets/javascripts/components/_body.js.jsx
@@ -8,6 +8,7 @@ class Body extends React.Component {
 
     {/* need to look at what/how bind is/works! */}
     this.handleNewItem = this.handleNewItem.bind(this);
+    this.handleItemDeletion = this.handleItemDeletion.bind(this);
   }
 
   componentDidMount() {
@@ -18,15 +19,26 @@ class Body extends React.Component {
   }
 
   handleNewItem(item) {
-    console.log(this.state);
     this.setState({ items: this.state.items.concat(item) });
+  }
+
+  handleItemDeletion(id){
+    $.ajax({
+      url: `/api/v1/items/${id}`,
+      type: 'DELETE',
+      success: () => {
+        this.setState({
+          items: this.state.items.filter((item) => { return item.id != id; })
+        });
+      }
+    });
   }
 
   render() {
     return (
       <div>
         <NewItem handleNewItem={this.handleNewItem} />
-        <AllItems items={this.state.items} />
+        <AllItems items={this.state.items} handleItemDeletion={this.handleItemDeletion} />
       </div>
     );
   }
