d1404586351f517087130afc10ff0da545950ad2
ronanduddy <ronanduddy@live.ie>
2018-03-05 20:00:20 +0000

    deploy... wip#

--- Capfile
+++ Capfile
@@ -39,6 +39,7 @@ require 'capistrano/bundler'
 require 'capistrano/rails/assets' # for asset handling add
 require 'capistrano/rails/migrations' # for running migrations
 require 'capistrano/puma'
+require 'capistrano/bower'
 
 # Load custom tasks from `lib/capistrano/tasks` if you have any defined
 Dir.glob("lib/capistrano/tasks/*.rake").each { |r| import r }
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server 'ec2-54-172-224-124.compute-1.amazonaws.com', user: 'deploy', roles: %w{web app db}
+server '54.84.92.2', user: 'deploy', roles: %w{web app db}
 
 
 # role-based syntax
