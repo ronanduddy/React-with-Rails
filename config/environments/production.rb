a3c76a8f3db309d45a28e4736bbc3e4facbfc0ea
ronanduddy <ronanduddy@live.ie>
2018-03-06 17:33:07 +0000

    Deploy assets wip

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,7 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   # config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
   config.serve_static_files = false
-  # config.assets.precompile += %w( *.js ^[^_]*.css *.css.erb )
+  config.assets.precompile += %w( *.js )
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
