0254b3f2e109856f6152def77f85948305c877fd
ronanduddy <ronanduddy@live.ie>
2018-03-06 17:31:53 +0000

    Deploy assets wip

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,8 +23,8 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   # config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
-  config.serve_static_assets = false
-  config.assets.precompile += %w( *.js ^[^_]*.css *.css.erb )
+  config.serve_static_files = false
+  # config.assets.precompile += %w( *.js ^[^_]*.css *.css.erb )
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
