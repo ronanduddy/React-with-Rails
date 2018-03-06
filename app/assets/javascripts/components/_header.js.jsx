87548647dfbca96d5c219f94b2b0eef1229cb57f
ronanduddy <ronanduddy@live.ie>
2018-03-06 18:11:12 +0000

    Finally getting deployed..

--- app/assets/javascripts/components/_header.js.jsx
+++ app/assets/javascripts/components/_header.js.jsx
@@ -3,7 +3,8 @@ class Header extends React.Component {
     return (
       <div className="row justify-content-center">
         <div className="col-6">
-          <h1>Hello, World!</h1>
+          <h1>Hello, React World!</h1>
+          <p className="float-right">Check out the <a target="_new" href="https://github.com/ronanduddy/React-with-Rails">code</a></p>
         </div>
       </div>
     );
